6ce21309a89acd2090291f465df3e6dc8684ba43
stellalee <stila917@gmail.com>
2022-04-18 19:41:25 +0900

    reducer

---
+++ src/todos/reducer.js
@@ -0,0 +1,52 @@
+import {
+    CREATE_TODO,
+    REMOVE_TODO,
+    LOAD_TODOS_SUCCESS,
+    LOAD_TODOS_IN_PROGRESS,
+    LOAD_TODOS_FAILURE
+} from './actions'
+
+export const isLoading = (state = false, action) => {
+    const {type} = action;
+
+    switch (type) {
+        case LOAD_TODOS_IN_PROGRESS :
+            return true;
+
+        case LOAD_TODOS_SUCCESS :
+        case LOAD_TODOS_FAILURE :
+            return false;
+
+        default:
+            return state;
+    }
+}
+
+export const todos = (state = [], action) => {
+    const {type, payload} = action;
+
+    switch (type) {
+        case CREATE_TODO : {
+            const {text} = payload
+            const newTodo = {
+                text,
+                isCompleted: false,
+            };
+            console.log('reducer: newTodo', newTodo);
+            return state.concat(newTodo)
+        }
+        case REMOVE_TODO : {
+            const {text} = payload
+            return state.filter((todo) => todo.text !== text)
+        }
+
+        case LOAD_TODOS_SUCCESS :{
+            const  {todos} = payload;
+            return todos;
+        }
+        case LOAD_TODOS_FAILURE :
+        case LOAD_TODOS_IN_PROGRESS :
+        default:
+            return state;
+    }
+}
