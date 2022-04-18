20f829d5b32812c371b1fcc136f74c51c47910ca
stellalee <stila917@gmail.com>
2022-04-18 19:06:37 +0900

    check it is loading if so show the loading msg

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
 import {removeTodo} from "./actions";
-
-
+import {loadTodos} from './thunk'
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
 import './styles/TodoList.css';
 
 
-const TodoList = ({todos = [], onRemovePressed}) => (
-    <div className="list-wrapper">
-        <NewTodoForm/>
-        {todos.map((todo, i) => <TodoListItem todo={todo}
-                                              id={i}
-                                              onRemovePressed={onRemovePressed}/>)}
-    </div>
-);
+
+const TodoList = ({todos = [], onRemovePressed, isLoading, startLoadingTodos}) => {
+
+    useEffect(() => {
+        startLoadingTodos();
+    },[])
+
+    const loadingMessage = <div>loading...</div>;
+    const content = (
+        <div className="list-wrapper">
+            <NewTodoForm/>
+            {todos.map((todo, i) => <TodoListItem todo={todo}
+                                                  id={i}
+                                                  onRemovePressed={onRemovePressed}
+            />)}
+        </div>
+    )
+    return isLoading ? loadingMessage : content;
+};
 
 
 // interacting state, so pass {todos} as props on this function
 const mapStateProps = (state) => ({
+    isLoading: state.isLoading,
     todos: state.todos,
 });
 
 // import action and dispatch action with props and pass as props on fn
 const mapDispatchProps = (dispatch) => ({
+    startLoadingTodos: () => dispatch(loadTodos()),
     onRemovePressed: text => dispatch(removeTodo(text)),
 })
 
