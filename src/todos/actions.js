71f51a2c73544ed3ff8c40262a5445bb00feafef
stellalee <stila917@gmail.com>
2022-04-18 19:06:08 +0900

    loading actions

--- src/todos/actions.js
+++ src/todos/actions.js
@@ -1,6 +1,10 @@
 export const CREATE_TODO = 'CREATE_TO';
 export const REMOVE_TODO = 'REMOVE_TODO';
 
+//thunks
+export const LOAD_TODOS_IN_PROGRESS = 'LOAD_TODOS_IN_PROGRESS';
+export const LOAD_TODOS_SUCCESS = 'LOAD_TODOS_SUCCESS';
+export const LOAD_TODOS_FAILURE = 'LOAD_TODOS_FAILURE';
 
 export const createTodo = (text) => ({
     type: CREATE_TODO,
@@ -10,4 +14,20 @@ export const createTodo = (text) => ({
 export const removeTodo = (text) => ({
     type: REMOVE_TODO,
     payload: {text},
+})
+
+
+export const loadTodosInProgress = () => ({
+    type: LOAD_TODOS_IN_PROGRESS,
+})
+
+export const loadTodosSuccess = (todos) => ({
+    type: LOAD_TODOS_SUCCESS,
+    payload: {todos},
+
+})
+
+export const loadTodosFailure = (todos) => ({
+    type: LOAD_TODOS_FAILURE,
+
 })
