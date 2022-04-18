759e1a350600d1b2be718eaaa0308e95df9e8fa3
stellalee <stila917@gmail.com>
2022-04-18 15:31:29 +0900

    added redux devtool enchancer

---
+++ src/store.js
@@ -0,0 +1,29 @@
+import { createStore, combineReducers } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+
+
+import {persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+
+
+import { todos } from './todos/reducer'
+//action, reducer
+
+
+const reducers = {
+    todos
+};
+
+
+const persistConfig ={
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2,
+}
+
+const rootReducer = combineReducers(reducers);
+const persisReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () => createStore(persisReducer, devToolsEnhancer());
